eba3a9c0eaf30775de95dec3e6aa4d30b45db07e
neeru0713 <neerurani1307@gmail.com>
2023-10-22 16:29:41 +0530

    card component in min view complete , carousel component completed with previous and next button functionality , transition effect for width to increase when clicked on card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { AiFillGithub, AiOutlineMessage } from "react-icons/ai";
+import Button from "./Button";
 
-const Card = ({ data }) => {
+const Card = ({ data, clickCardHandler, index, showCard, clickedCard }) => {
+  const cardRef = useRef(null);
   const [isHovered, setIsHovered] = useState(false);
- function getUserName(email) {
-   const userName = email.split("@")[0];
-   return userName.charAt(0).toUpperCase() + userName.slice(1);
- }
 
+  useEffect(() => {
+  }, [clickedCard])
+
+  function getUserName(email) {
+    const userName = email.split("@")[0];
+    return userName.charAt(0).toUpperCase() + userName.slice(1);
+  }
 
   function getProjectImage() {
     let file = "";
@@ -21,64 +26,102 @@ const Card = ({ data }) => {
     return file;
   }
 
-    function getSomeText() {
-        let text = ""
-        data.fields.forEach((field) => {
-            if (field.type === "text") {
-              text = field.value.substr(0,100)
-          }
-        })
-        return text;
+  function cardClickHandler() {
+    cardRef.current.style.width = "95vw";
+    clickCardHandler(index);
   }
 
+  function getSomeText() {
+    let text = "";
+    data.fields.forEach((field) => {
+      if (field.type === "text") {
+        text = field.value.substr(0, 100);
+      }
+    });
+    return text;
+  }
+
+  function getBgOfCard() {
+    if (clickedCard === index) {
+      return {
+        background: "transparent",
+      };
+    } else {
+      return {
+        backgroundImage: `url("data:image/png;base64,${getProjectImage()}")`,
+      };
+    }
+  }
+
+  const backButtonHandler = (e) => {
+    e.stopPropagation()
+    cardRef.current.style.width = "100%";
+    clickCardHandler(-1);
+  };
+
   return (
-    <div
-      class="card-parent relative rounded-[2rem] w-[100%] h-[36rem] shadow-lg overflow-hidden cursor-pointer inline-block hover:shadow-orange-500/80"
-      onMouseEnter={(e) => {
-        setIsHovered(true);
-      }}
-      onMouseLeave={(e) => {
-        setIsHovered(false);
-      }}
-    >
-      <div class="card-overlay border rounded-[2rem] absolute inset-0 bg-black opacity-0 transition flex flex-col-reverse ">
-        {isHovered && (
-          <>
-            <div
-              id="card-content"
-              className="flex flex-col justify-between w-[100%] p-2 mb-4 opacity-100"
-            >
-              <div className="flex w-[24%] justify-between opacity-100">
-                <div>
-                  <a href={data.projectUrl} target="_blank">
-                    <AiFillGithub className="h-7 w-7 opacity-100" />
-                  </a>
+    <>
+      {showCard && (
+        <div
+          ref={cardRef}
+          onClick={cardClickHandler}
+          class="cursor-pointer card-parent relative rounded-[2rem] w-[100%] h-[38vw] shadow-lg overflow-hidden cursor-pointer inline-block hover:shadow-orange-500/80 transition-width duration-500 ease-in-out "
+          onMouseEnter={(e) => {
+            setIsHovered(true);
+          }}
+          onMouseLeave={(e) => {
+            setIsHovered(false);
+          }}
+        >
+          {/* overlay with short view code */}
+          {clickedCard !== index && isHovered && (
+            <div class="card-overlay border rounded-[2rem] absolute inset-0 bg-black opacity-0 transition flex flex-col-reverse ">
+              <div
+                id="card-content"
+                className="flex flex-col justify-between w-[100%] p-2 mb-4 opacity-100"
+              >
+                <div className="flex w-[24%] justify-between opacity-100">
+                  <div>
+                    <a href={data.projectUrl} target="_blank">
+                      <AiFillGithub className="h-7 w-7 opacity-100" />
+                    </a>
+                  </div>
+                  <div>
+                    {" "}
+                    <AiOutlineMessage className="h-7 w-7 opacity-100" />{" "}
+                  </div>
                 </div>
-                <div>
-                  {" "}
-                  <AiOutlineMessage className="h-7 w-7 opacity-100" />{" "}
-                </div>
-              </div>
 
-              <div className="font-bold text-3xl opacity-100 my-2">
-                {data.title}
+                <div className="font-bold text-3xl opacity-100 my-2">
+                  {data.title}
+                </div>
+                <hr className="w-[90%] mt-2 mb-1 border-1 border-orange-700 shadow-lg shadow shadow-orange-500/80" />
+                <div>{getSomeText()}</div>
               </div>
-              <hr className="w-[90%] mt-2 mb-1 border-1 border-orange-700 shadow-lg shadow shadow-orange-500/80" />
-              <div>{getSomeText()}</div>
+              <div className="absolute top-0 right-[6%] italic mt-2 text-md font-semibold">
+                {getUserName(data?.user?.email)}
+              </div>
             </div>
-            <div className="absolute top-0 right-[6%] italic mt-2 text-md font-semibold">{getUserName(data?.user?.email)}</div>
-          </>
-        )}
-      </div>
+          )}
 
-      <div
-        id="card"
-        className="card-child border rounded-[2rem] h-full w-full flex flex-col bg-cover bg-center bg-no-repeat "
-        style={{
-          backgroundImage: `url('data:image/png;base64,${getProjectImage()}')`,
-        }}
-      ></div>
-    </div>
+          <div
+            id="card"
+            className={` ${
+              clickedCard === index ? "" : "card-child"
+            } border rounded-[2rem] h-full w-full flex flex-col bg-cover bg-center bg-no-repeat border-2 border-teal-600`}
+            style={getBgOfCard()}
+          >
+            {clickedCard === index && <span onClick={backButtonHandler} className="text-black text-[50px] text-teal-600 absolute top-[-15px] left-2">&lt;</span>}
+          </div>
+        </div>
+      )}
+
+      {/* {clickedCard === index && (
+        <div class="card-parent relative rounded-[2rem] w-[100%] h-[38vw] shadow-lg overflow-hidden cursor-pointer inline-block hover:shadow-orange-500/80">
+          Hello
+        </div>
+      )} */}
+    </>
   );
 };
 
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,44 +1,95 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
+import {
+  IoChevronBackCircleOutline,
+  IoChevronForwardCircleOutline,
+} from "react-icons/io5";
 export const Carousel = ({ cards }) => {
   const [startIndex, setStartIndex] = useState(0);
   const [endIndex, setEndIndex] = useState(4);
+  const [clickedCard, setClickCard] = useState(-1);
+  const [filteredCards, setFilteredCards] = useState();
 
-  const handleLeftClick = () => {
-    setStartIndex((prev) => {
-      return prev === 0 ? 0 : prev - 1;
-    });
-    setEndIndex((prev) => {
-      return prev === 0 ? 0 : prev - 1;
-    });
-  };
+  useEffect(() => {
+  
+      setFilteredCards(cards);
+    
+  }, [cards]);
+
+   const handleLeftClick = () => {
+     setStartIndex((prev) => {
+       return prev - 1;
+     });
+     setEndIndex((prev) => {
+       return prev - 1;
+     });
+
+     let temp = [];
+
+     for (let i = startIndex - 1; i <= endIndex - 1; i++) {
+       
+       let j = i;
+     
+       temp.push(cards[j]);
+     }
+
+    
+     setFilteredCards(temp);
+   };
 
   const handleRightClick = () => {
     setStartIndex((prev) => {
-      return prev === cards.length - 1 ? cards.length - 1 : prev + 1;
+      return prev + 1;
     });
     setEndIndex((prev) => {
-      return prev === cards.length - 1 ? cards.length - 1 : prev + 1;
+      return prev + 1;
     });
+    let temp = [];
+    
+    for (let i = startIndex+1; i <= endIndex+1; i++) {
+
+      let j = i;
+      if (i >= cards.length) {
+        j = i % cards.length;
+      }
+      temp.push(cards[j]);
+    }
+    setFilteredCards(temp)
   };
 
-  return (
-    <div
-      className="carousel flex items-center w-[100%] pt-[6rem]"
-    >
-      <button onClick={handleLeftClick} className="arrow left text-[50px] mr-8">
-        &#8249;
-      </button>
+  function clickCardHandler(index) {
+    setClickCard(index);
+  }
 
+  return (
+    <div className="carousel flex items-center w-[100%] pt-[6rem] px-8">
+      {startIndex > 0 && clickedCard === -1 && (
+        <button
+          onClick={handleLeftClick}
+          className="arrow left text-[50px] mr-8 absolute zindex-custom-2 bg-black border left-0 rounded-[100px]"
+        >
+          <IoChevronBackCircleOutline />
+        </button>
+      )}
       <div className="grid grid-cols-5 gap-8 w-[100%]">
         {cards?.slice(startIndex, endIndex + 1).map((card, index) => (
-          <Card data={card} />
+          <Card
+            data={card}
+            clickCardHandler={clickCardHandler}
+            index={index}
+            clickedCard={clickedCard}
+            showCard={clickedCard === -1 || clickedCard === index}
+          />
         ))}
       </div>
-
-      <button onClick={handleRightClick} className="arrow right text-[50px]">
-        &#8250;
-      </button>
+      {endIndex < cards?.length - 1 && clickedCard === -1 && (
+        <button
+          onClick={handleRightClick}
+          className="arrow right text-[50px] arrow absolute right-0 zindex-custom-2 bg-black border rounded-[100px]"
+        >
+          <IoChevronForwardCircleOutline />
+        </button>
+      )}
     </div>
   );
 };
